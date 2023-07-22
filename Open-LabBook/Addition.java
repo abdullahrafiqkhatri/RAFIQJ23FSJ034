838907098e84253996d5084cfb9d8ce04359c410
Abdullah Khatri <63718615+abdullahrafiqkhatri@users.noreply.github.com>
2023-07-22 07:27:10 +0530

    Add files via upload

---
+++ Open-LabBook/Addition.java
@@ -0,0 +1,11 @@
+package basicsOLB;
+
+public class Addition {
+	public static void main(String[] args) {
+		int first = 56;
+		int second = 44;
+		int sum = first + second;
+		System.out.println(first + " + " +second+ 
+				"\nThe sum is: "+sum);
+	}
+}
