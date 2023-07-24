b90cf7669581fd69dfbfcf8ce71606201857f8a3
Abdullah Khatri <63718615+abdullahrafiqkhatri@users.noreply.github.com>
2023-07-24 17:54:27 +0530

    Update CommandLineArgument.java

--- Closed-LapBook/CommandLineArgument.java
+++ Closed-LapBook/CommandLineArgument.java
@@ -1,5 +1,14 @@
 package basicsCLB;
-
+/* 
+ * Right click on file or on screen select run as -> run configuration ->  go to argument section 
+ * -> You can provide the arguments separated by spaces. ->For example, 
+ * if you provide the following command-line arguments: "hello world 123", 
+ * the output will be:
+ * Argument 1: hello
+ * Argument 2: a
+ * Argument 3: world
+ * Argument 4: 123
+ */
 public class CommandLineArgument {
 	public static void main(String[] args) {
 		if(args.length == 0) {
