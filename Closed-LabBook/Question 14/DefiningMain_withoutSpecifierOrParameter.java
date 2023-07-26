f13006c34e6d80ec0a182794d4c44261c38c1292
Abdullah Khatri <63718615+abdullahrafiqkhatri@users.noreply.github.com>
2023-07-26 17:02:45 +0530

    Add files via upload

---
+++ Closed-LabBook/Question 14/DefiningMain_withoutSpecifierOrParameter.java
@@ -0,0 +1,30 @@
+
+package basicsCLB.Question14;
+
+public class DefiningMain_withoutSpecifierOrParameter {
+	public static void main() {
+		System.out.println("Hello Java");
+	}
+	
+	/* a) Without public specifier----------->
+	 * 
+	 * Error: Main method not found in class basicsCLB.Question14.DefiningMain_withoutSpecifierOrParameter,
+	 *  please define the main method as:
+	 * public static void main(String[] args)
+	 */
+	
+	/* b) Without static specifier----------->
+	 * 
+	 * Error: Main method is not static in class basicsCLB.Question14.DefiningMain_withoutSpecifierOrParameter,
+	 *  please define the main method as:
+	 * public static void main(String[] args)
+	 */
+	
+	/* b) Without static specifier----------->
+	 * 
+	 * Error: Main method not found in class basicsCLB.Question14.DefiningMain_withoutSpecifierOrParameter,
+	 *  please define the main method as:
+	 * public static void main(String[] args)
+	 */
+
+}
