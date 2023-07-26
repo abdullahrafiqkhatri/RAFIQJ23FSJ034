db44235c08da4f3bcfc7415dcbefec52d81d5d8d
Abdullah Khatri <63718615+abdullahrafiqkhatri@users.noreply.github.com>
2023-07-26 16:55:24 +0530

    Add files via upload

---
+++ Closed-LabBook/Question 14/DifferentClassName.java
@@ -0,0 +1,12 @@
+
+package basicsCLB.Question14;
+
+/*
+ * Error: Could not find or load main class basicsCLB.Question14.DifferentClassName
+		Caused by: java.lang.ClassNotFoundException: basicsCLB.Question14.DifferentClassName
+ */
+public class GaveDifferentClassName {
+	public static void main(String[] args) {
+		System.out.println("Hello Java");
+	}
+}
