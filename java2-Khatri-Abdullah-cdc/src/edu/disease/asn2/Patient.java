342cead36f775fdaa8d83b6d917d8ee18ff3a40d
Abdullah Khatri <63718615+abdullahrafiqkhatri@users.noreply.github.com>
2023-08-21 15:56:25 +0530

    Update Patient.java

--- java2-Khatri-Abdullah-cdc/src/edu/disease/asn2/Patient.java
+++ java2-Khatri-Abdullah-cdc/src/edu/disease/asn2/Patient.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 
 import edu.disease.asn1.Exposure;
 /**
- * 
+ * Represents a patient with exposures and disease IDs.
  */
 public class Patient implements Comparable<Patient> {
 	private UUID patientID;
@@ -19,8 +19,12 @@ public class Patient implements Comparable<Patient> {
 	private UUID[] diseaseIDs;
 	
 	/**
-	 * 
-	 */
+     * Constructor for creating a Patient instance.
+     *
+     * @param maxDiseases   Maximum number of diseases.
+     * @param maxExposures  Maximum number of exposures.
+     * @throws IllegalArgumentException If maxDiseases or maxExposures are non-positive.
+     */
 	public Patient(int maxDiseases, int maxExposures) {
 		if(maxDiseases<=0 && maxExposures <=0) {
 			throw new IllegalArgumentException("maxDiseases and maxExposure must be greater than zero");
@@ -28,7 +32,12 @@ public class Patient implements Comparable<Patient> {
 		diseaseIDs = new UUID[maxDiseases];
 		exposures = new Exposure[maxExposures];
 	}
-	
+	/**
+     * Adds a disease ID to the diseaseIds array.
+     *
+     * @param diseaseId The UUID of the disease.
+     * @throws IndexOutOfBoundsException If the diseaseIds array is full.
+     */
 	public void addDiseaseID(UUID diseaseID) {
 		for(int i=0; i<diseaseIDs.length; i++) {
 			if (diseaseIDs[i] == null) {
@@ -39,6 +48,12 @@ public class Patient implements Comparable<Patient> {
 		throw new IndexOutOfBoundsException("Disease IDs array is FULL!");
 	}
 	
+	/**
+     * Adds an exposure to the exposures array.
+     *
+     * @param exposure The exposure to add.
+     * @throws IndexOutOfBoundsException If the exposures array is full.
+     */
 	public void addExposure(Exposure exposure) {
 		for(int i=0; i<exposures.length; i++) {
 			if(exposures[i]== null ) {
@@ -144,7 +159,12 @@ public class Patient implements Comparable<Patient> {
                 ", lastName='" + lastName + '\'' +
                 '}';
 	}
-	
+	/**
+     * Compare patients for sorting in ascending order by last name, then first name.
+     *
+     * @param other The other patient to compare to.
+     * @return Negative if this patient comes before the other, positive if after, 0 if equal.
+     */
 	@Override
     public int compareTo(Patient other) {
         Comparator<Patient> lastNameComparator = Comparator.comparing(Patient::getLastName, String.CASE_INSENSITIVE_ORDER)
